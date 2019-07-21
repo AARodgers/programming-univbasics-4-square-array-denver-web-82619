40c63be620ed7532cf622a5d6e74d9a1829703ea
Amanda Rodgers <aarodgerswork@gmail.com>
2019-07-21 19:48:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
-def square_array(array)
-  # your code here
+def output_array_elements(array)
+  array = []
+  new_array = []
+ counter = 0
+ 
+ while counter < array.length do
+   puts array.each { |n| puts n*n }
+   counter += 1
+ new_array = array.each { |n| puts n*n }
+ new_array
+ end
 end
+
+
+
+ 
+ 
